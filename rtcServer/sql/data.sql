857d1df063f667f1bad2e171cac313c676886619
dazlX <netyemails@gmail.com>
2025-06-02 20:51:48 +0300

    sql

---
+++ rtcServer/sql/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO cam(namecam, location, latitude, longitude, camId)
+VALUES
+  ('Sloth TV Playground Cam', 'Heredia Province, Costa Rica', 10.025969715068793, -84.03511411686519, 'https://outbound-production.explore.org/stream-production-560/.m3u8'),
+  ('Sandy Channel Underwater Reef Cam', 'Utopia Village on Utila, The Bay Islands, Honduras', 16.072748292764455, -86.9512014897999, 'https://outbound-production.explore.org/stream-production-720/.m3u8'),
+  ('Tropical Reef Aquarium', 'Long Beach, California, USA', 33.76834413389795, -118.19452309357976, 'https://outbound-production.explore.org/stream-production-10/.m3u8'),
+  ('Decorah North Eagles', 'Decorah, Iowa, USA', 43.30333198863682, -91.78616408721277, 'https://outbound-production.explore.org/stream-production-215/.m3u8'),
+  ('Trempealeau Eagles', 'Trempealeau, Wisconsin', 43.332419204498855, -91.78803683765146, 'https://outbound-production.explore.org/stream-production-797/.m3u8'),
+  ('Tammy the Cheetah Cam at HESC', 'Hoedspruit, South Africa', -24.51353154246984, 31.0323559632421, 'https://outbound-production.explore.org/stream-production-803/.m3u8'),
+  ('Tembe Elephant Park', 'Emangusi, South Africa', -24.196956728292232, 30.908641938224157, 'https://outbound-production.explore.org/stream-production-110/.m3u8');
